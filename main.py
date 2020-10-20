224c1838a8b98279b8920706b3859838c3c61eba
Evelin Rodriguez Suarez <evelin992009@gmail.com>
2020-10-20 20:20:19 +0000

    comentarios

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 print ("escriba nombre")
 nombre = input()
 print ("hola" + nombre +"!")
+#aca se ve como pide nombre dentro de input
+nombre1 = input("escriba su nombre")
+print ("hola" + nombre + "!")
 
+# para hacer comentarios se utliza eso o tres comillas arriba , luego mensaje y al final otras tre comillas
